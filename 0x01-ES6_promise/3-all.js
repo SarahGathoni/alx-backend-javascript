62d82c6317c060db7b8a5f722a62b058ff02c871
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-06-29 19:57:03 +0300

    changes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,19 @@
+import {uploadPhoto, createUser} from './utils';
+
 export default function handleProfileSignup(firstName, lastName){
-    const promise = new Promise((resolve, reject) =>{
-        const names = {
-            firstName,
-            lastName
-        }
-        resolve(names.firstName);
-        resolve(names.lastName);
+
+    const promise1 = new Promise((resolve, reject) =>{
+        resolve(firstName);
+    });
+
+    const promise2 = new Promise((resolve, reject) =>{
+        resolve(lastName);
+    });
+    promise.all(promise1, promise2)
+    .then((message) =>{
+        console.log('body firstName lastName' +message)
+    }).catch((error)=>{
+        console.log(error)
     })
-    promise.all(console.log('body firstName lastName'))
-    reject('Signup system offline ')
     
 }
