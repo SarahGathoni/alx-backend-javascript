31bd26f336cf81b26e8eedb8dcee3cc79dc74e0a
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-08-24 18:27:36 +0000

    updates

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
-	process.stdin.on("data", (data) => {
-		process.stdout.write(`Your name is: ${data.toString()}`);
-		process.exit();
-	});
-} else {
-	process.stdin.on("data", (data) => {
-		process.stdout.write(`Your name is: ${data.toString()}`);
-		process.exit();
-	});
-	process.on("exit", ()=> {
-		process.stdout.write("This important software is closing now\n");
-	});
-}
+process.stdin.on('readable', () => {
+  const i = process.stdin.read();
+
+  if (i) {
+    process.stdout.write(`Your name is: ${i}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
