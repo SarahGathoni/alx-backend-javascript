c5ed1079d48fa75c74149b49e7955c02f3cf5ff1
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-09-18 22:58:20 -0700

    es6 promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,17 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        const response = {
-          status: 200,
-          body: 'Success',
-        };
-        resolve(response);
-      } else {
-        const error = new Error('The fake API is not working currently');
-        reject(error);
-      }
-    });
-  }
-  
-    
-            
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
