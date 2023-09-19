9fa54c56e0e6a599320e589dbb8d10623b609be1
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-09-19 00:03:12 -0700

    es6 basics

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    var task;
-    var task2;
-  
-    if (trueOrFalse) {
-      var task = true;/* eslint-disable-line no-unused-vars */
-      var task2 = false;/* eslint-disable-line no-unused-vars */
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
+
+  return [task, task2];
+}
